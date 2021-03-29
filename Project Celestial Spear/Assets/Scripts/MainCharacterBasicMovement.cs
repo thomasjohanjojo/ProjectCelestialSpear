c9324d2d126ded8b9094740293edaf0c2fd978d4
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-03-29 17:05:05 +0530

    Player Movement somewhat okay
    player movement is upto prototype standards

--- Project Celestial Spear/Assets/Scripts/MainCharacterBasicMovement.cs
+++ Project Celestial Spear/Assets/Scripts/MainCharacterBasicMovement.cs
@@ -27,7 +27,7 @@ public class MainCharacterBasicMovement : MonoBehaviour
     void Start()
     {
         //Default Values
-        playerSpeed = 5;
+        
         canMove = true;
         isMoving = false;
         playerHorizontalInputValue = 0;
@@ -41,7 +41,9 @@ public class MainCharacterBasicMovement : MonoBehaviour
     void Update()
     {
         obtainMovementInputFromPlayer();
+        flipPlayerFacingDirectionAccordingToDirectionOfInput();
         movePlayerHorizontally();
+
         
     }
 
@@ -59,5 +61,39 @@ public class MainCharacterBasicMovement : MonoBehaviour
         {
             maincharacterRigidbody.velocity = new Vector3(0f, maincharacterRigidbody.velocity.y, 0f);
         }
+
+        if(Mathf.Abs( maincharacterRigidbody.velocity.x ) > playerSpeed)
+        {
+            if(playerHorizontalInputValue > 0)
+            {
+                maincharacterRigidbody.velocity = new Vector3(playerSpeed, maincharacterRigidbody.velocity.y, 0f);
+            }
+
+            else
+            {
+                maincharacterRigidbody.velocity = new Vector3(-playerSpeed, maincharacterRigidbody.velocity.y, 0f);
+            }
+        }
+    }
+
+    void flipPlayerFacingDirectionAccordingToDirectionOfInput()
+    {
+        
+        if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, 0f, transform.rotation.z);
+            
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            gameObject.transform.rotation = Quaternion.Euler(transform.rotation.x, 180f, transform.rotation.z);
+           
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 0)
+        {
+            gameObject.transform.localScale = new Vector3(gameObject.transform.localScale.x, gameObject.transform.localScale.y, gameObject.transform.localScale.z);
+        }
+
     }
 }
