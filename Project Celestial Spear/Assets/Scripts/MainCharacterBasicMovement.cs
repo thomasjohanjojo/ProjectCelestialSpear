a90a186cc0dd5972409f4ae5b63f4941e2109ba9
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-03-29 14:54:38 +0530

    Trivial commit for movement code
    added basic fields

--- Project Celestial Spear/Assets/Scripts/MainCharacterBasicMovement.cs
+++ Project Celestial Spear/Assets/Scripts/MainCharacterBasicMovement.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class MainCharacterBasicMovement : MonoBehaviour
 {
+    /// <summary>
+    /// Basic player movement for moving left and right at a moderate speed with other variables for increased manipulation
+    /// </summary>
+
+    //Fields
+    public float playerSpeed;
+    public bool canMove;
+    public bool isMoving;
+    private float playerInputValue; 
+
+
+
+
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
+        //Default Values
+        playerSpeed = 5;
+        canMove = true;
+        isMoving = false;
+        playerInputValue = 0;
         
     }
 
