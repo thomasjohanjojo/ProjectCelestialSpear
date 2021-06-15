644b78ab34d9f1a30290a680f9cbd1d5fc3a9bf2
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-06-15 23:26:27 +0530

    Max distance for axe throw added and functioning
    The axe will now start its return journey after travelling a particular distance

--- Project Celestial Spear/Assets/Scripts/AxeThrowScript.cs
+++ Project Celestial Spear/Assets/Scripts/AxeThrowScript.cs
@@ -11,6 +11,15 @@ public class AxeThrowScript : MonoBehaviour
 
     public Transform transformOfThisAxe;
 
+    public Vector3 positionOfTheMouseClick;
+
+    public float maxDistanceOfAxeTravel;
+
+    private Vector3 directionToThePlayerWithMagnitude;
+
+    public bool goBackToThePlayerAfterAttack = true;
+    public bool goToTheEnemyToAttack = false;
+
 
 
     // Start is called before the first frame update
@@ -22,12 +31,58 @@ public class AxeThrowScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // calculate the direction and normalize it and move towards it.
-        directionToThePlayer = transformOfThePlayer.position - transformOfThisAxe.position;
-        directionToThePlayer.Normalize();
-        transform.Translate(directionToThePlayer * Time.deltaTime);
+        CheckDistanceToPlayer();
+
+        if(goToTheEnemyToAttack == true)
+        {
+            transform.Translate(positionOfTheMouseClick * Time.deltaTime);
+            CheckIfAxeHasTravelledMaximumDistanceAndIfSoThenGoBackToPlayer();
+            
+        }
+
+        else if(goBackToThePlayerAfterAttack == true)
+        {
+            GoBackToThePlayer();
+        }
+
+        DetectMousePositionWheneverFire2ButtonIsPressed();
 
     }
 
+    public void DetectMousePositionWheneverFire2ButtonIsPressed()
+    {
+        if(Input.GetButtonDown("Fire2"))
+        {
+            positionOfTheMouseClick = Input.mousePosition;
+            positionOfTheMouseClick = Camera.main.ScreenToWorldPoint(positionOfTheMouseClick);
+            positionOfTheMouseClick.z = transform.position.z;
+            positionOfTheMouseClick.Normalize();
+            goBackToThePlayerAfterAttack = false;
+            goToTheEnemyToAttack = true;
+            
+        }
+    }
+
+    public void CheckDistanceToPlayer()
+    {
+        directionToThePlayer = transformOfThePlayer.position - transformOfThisAxe.position;
+        directionToThePlayerWithMagnitude = directionToThePlayer;
+    }
+
+    public void GoBackToThePlayer()
+    {
+        
+        directionToThePlayer.Normalize();
+        transform.Translate(directionToThePlayer * Time.deltaTime);
+    }
+
+    public void CheckIfAxeHasTravelledMaximumDistanceAndIfSoThenGoBackToPlayer()
+    {
+        if(directionToThePlayerWithMagnitude.magnitude > maxDistanceOfAxeTravel)
+        {
+            goToTheEnemyToAttack = false;
+            goBackToThePlayerAfterAttack = true;
+        }
+    }
     
 }
