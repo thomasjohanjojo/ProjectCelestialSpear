21f619092032b7dcd99f0567e3ce83eab395a432
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-06-16 01:32:58 +0530

    Still  only 90% has been completed by functionality has been somewhat restored
    the only problem left is that the axe still keeps moving even after TELEPORTING back to the player

--- Project Celestial Spear/Assets/Scripts/AxeThrowScript.cs
+++ Project Celestial Spear/Assets/Scripts/AxeThrowScript.cs
@@ -102,6 +102,8 @@ public class AxeThrowScript : MonoBehaviour
         if(directionToThePlayerWithMagnitude.magnitude < 0.5)
         {
             pressingTheRangedAttackButtonIsPossibleAndCanThrowAxe = true;
+            axeThrowHasBeenPressedOnceBeforeReturnJourney = false;
+            goBackToThePlayerAfterAttack = false;
             
         }
     }
