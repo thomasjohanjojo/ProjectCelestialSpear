2e06248fd6df9ef4db0956e63c868499a319b5d6
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-06-23 04:56:40 +0530

    Added keep rotation as same to axe throw raw script

--- Project Celestial Spear/Assets/Scripts/Axe/AxeThrowRaw.cs
+++ Project Celestial Spear/Assets/Scripts/Axe/AxeThrowRaw.cs
@@ -4,15 +4,166 @@ using UnityEngine;
 
 public class AxeThrowRaw : MonoBehaviour
 {
+    public Transform transformOfThePlayer;
+
+    public Vector3 directionToThePlayer;
+
+    public Transform transformOfThisAxe;
+
+    public Vector3 positionOfTheMouseClick;
+
+    public float maxDistanceOfAxeTravel;
+
+    public float speedOfAxeThrow;
+
+    public BoxCollider2D colliderOfTheAxe;
+
+    private Vector3 directionToThePlayerWithMagnitude;
+
+    public bool goBackToThePlayerAfterAttack = false;
+    public bool goToTheEnemyToAttack = false;
+    public bool pressingTheRangedAttackButtonIsPossibleAndCanThrowAxe = true;
+
+    public bool axeThrowHasBeenPressedOnceBeforeReturnJourney = false;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        goBackToThePlayerAfterAttack = false;
+        goToTheEnemyToAttack = false;
+        pressingTheRangedAttackButtonIsPossibleAndCanThrowAxe = true;
+        axeThrowHasBeenPressedOnceBeforeReturnJourney = false;
+        colliderOfTheAxe.enabled = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckDistanceToPlayer();
+        KeepRotationAsSame();
+
+
+        if (goToTheEnemyToAttack == true)
+        {
+            transform.Translate(positionOfTheMouseClick * Time.deltaTime * speedOfAxeThrow);
+            CheckIfAxeHasTravelledMaximumDistanceAndIfSoThenGoBackToPlayer();
+
+        }
+
+        else if (goBackToThePlayerAfterAttack == true)
+        {
+            GoBackToThePlayer();
+            pressingTheRangedAttackButtonIsPossibleAndCanThrowAxe = false;
+            CheckIfDistanceToPlayerHasReachedZeroAndIfSoThenReEnableAxeThrow();
+
+        }
+
+        if (pressingTheRangedAttackButtonIsPossibleAndCanThrowAxe == true)
+        {
+            if (axeThrowHasBeenPressedOnceBeforeReturnJourney == false && goBackToThePlayerAfterAttack == false)
+            {
+                DetectMousePositionWheneverFire2ButtonIsPressed();
+            }
+
+            else if (axeThrowHasBeenPressedOnceBeforeReturnJourney == true && goBackToThePlayerAfterAttack == false)
+            {
+                DetectRangedAttackButtonPressAndInstantlyTransportTheAxeBackToThePlayer();
+            }
+        }
+    }
+
+    public void KeepRotationAsSame()
+    {
+        transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+    }
+
+    public void DetectMousePositionWheneverFire2ButtonIsPressed()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            positionOfTheMouseClick = Input.mousePosition;
+            positionOfTheMouseClick = Camera.main.ScreenToWorldPoint(positionOfTheMouseClick);
+            positionOfTheMouseClick.z = transform.position.z;
+            positionOfTheMouseClick.Normalize();
+            goBackToThePlayerAfterAttack = false;
+            goToTheEnemyToAttack = true;
+
+            axeThrowHasBeenPressedOnceBeforeReturnJourney = true;
+
+            colliderOfTheAxe.enabled = true;
+
+        }
+    }
+
+    public void DetectRangedAttackButtonPressAndInstantlyTransportTheAxeBackToThePlayer()
+    {
+        if (Input.GetButtonDown("Fire2"))
+        {
+            transform.position = transformOfThePlayer.position;
+            axeThrowHasBeenPressedOnceBeforeReturnJourney = false;
+            goBackToThePlayerAfterAttack = false;
+            goToTheEnemyToAttack = false;
+            colliderOfTheAxe.enabled = false;
+        }
+    }
+
+    public void CheckDistanceToPlayer()
+    {
+        directionToThePlayer = transformOfThePlayer.position - transformOfThisAxe.position;
+        directionToThePlayerWithMagnitude = directionToThePlayer;
+    }
+
+    public void CheckIfDistanceToPlayerHasReachedZeroAndIfSoThenReEnableAxeThrow()
+    {
+        if (directionToThePlayerWithMagnitude.magnitude < 0.5)
+        {
+            pressingTheRangedAttackButtonIsPossibleAndCanThrowAxe = true;
+            axeThrowHasBeenPressedOnceBeforeReturnJourney = false;
+            goBackToThePlayerAfterAttack = false;
+            goToTheEnemyToAttack = false;
+
+            colliderOfTheAxe.enabled = false;
+
+        }
+    }
+
+    public void GoBackToThePlayer()
+    {
+
+        directionToThePlayer.Normalize();
+        transform.Translate(directionToThePlayer * Time.deltaTime * speedOfAxeThrow);
+    }
+
+    public void CheckIfAxeHasTravelledMaximumDistanceAndIfSoThenGoBackToPlayer()
+    {
+        if (directionToThePlayerWithMagnitude.magnitude > maxDistanceOfAxeTravel)
+        {
+            goToTheEnemyToAttack = false;
+            goBackToThePlayerAfterAttack = true;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Ground")
+        {
+            GoBackToThePlayerIntantlySinceTheAxeHasTouchedGroundLayer();
+        }
+
+        else if (collision.tag == "Enemy")
+        {
+            // Deal Damage
+        }
+    }
+
+    public void GoBackToThePlayerIntantlySinceTheAxeHasTouchedGroundLayer()
+    {
+        transform.position = transformOfThePlayer.position;
+        axeThrowHasBeenPressedOnceBeforeReturnJourney = false;
+        goBackToThePlayerAfterAttack = false;
+        goToTheEnemyToAttack = false;
+        colliderOfTheAxe.enabled = false;
     }
 }
