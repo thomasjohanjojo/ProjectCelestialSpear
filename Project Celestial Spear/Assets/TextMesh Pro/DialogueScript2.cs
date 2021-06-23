f4bf49f1f161a9696848a3e1272bf7023e427314
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-06-23 18:14:04 +0530

    trivial

---
+++ Project Celestial Spear/Assets/TextMesh Pro/DialogueScript2.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DialogueScript2 : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public GameObject textBox;
+    public string[] sentences;
+    private int index;
+    public float typingSpeed;
+
+    public GameObject continueButton;
+    // public GameObject objectDestroy;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (textDisplay.text == sentences[index])
+        {
+            continueButton.SetActive(true);
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.tag == "Player")
+        {
+            textBox.SetActive(true);
+            StartCoroutine(Type());
+        }
+    }
+
+
+
+    public IEnumerator Type()
+    {
+        foreach (char letter in sentences[index].ToCharArray())
+        {
+            textDisplay.text += letter;
+            yield return new WaitForSeconds(typingSpeed);
+
+        }
+    }
+
+    public void NextSentence()
+    {
+        if (index < sentences.Length - 1)
+        {
+            index++;
+            textDisplay.text = "";
+            StartCoroutine(Type());
+        }
+        else
+        {
+            textDisplay.text = "";
+            textBox.SetActive(false);
+        }
+    }
+}
