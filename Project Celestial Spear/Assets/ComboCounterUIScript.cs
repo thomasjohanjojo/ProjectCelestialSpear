312d78995274cc860da37114e778d1142762a547
Thomas Johan Jojo <thomasjohanjojo@gmail.com>
2021-06-24 16:11:02 +0530

    Combo counter script imported but there is an error

---
+++ Project Celestial Spear/Assets/ComboCounterUIScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ComboCounterUIScript : MonoBehaviour
+{
+    [SerializeField] public PlayerAttack player;
+    public TextMeshProUGUI textMeshPro;
+
+    public int hitCount;
+
+    public void Start()
+    {
+        textMeshPro = GetComponent<TextMeshProUGUI>();
+        hitCount = 0;
+
+
+    }
+    public void Update()
+    {
+
+        {
+            // Debug.Log("Entered");
+            hitCount++;
+            SetHitCounter(player.HitCounterInt);
+
+        }
+    }
+    public void SetHitCounter(int hitCount)
+    {
+
+        textMeshPro.SetText(hitCount.ToString());
+    }
+}
